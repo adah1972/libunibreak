eac0372987a517d5d3d3785f926b556fab4dc42a
Wu Yongwei <wuyongwei@gmail.com>
2013-11-10 12:59:12 +0800

    Consistently avoid literal e-mail addresses in the source code.

--- src/wordbreak.c
+++ src/wordbreak.c
@@ -4,7 +4,7 @@
  * Word breaking in a Unicode sequence.  Designed to be used in a
  * generic text renderer.
  *
- * Copyright (C) 2013 Tom Hacohen <tom@stosb.com>
+ * Copyright (C) 2013 Tom Hacohen <tom at stosb dot com>
  *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the author be held liable for any damages
