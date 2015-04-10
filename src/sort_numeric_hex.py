5bdfde1e7450dd0257bef8339637f2adaa287bcd
Tom Hacohen <tom@stosb.com>
2015-04-10 11:59:04 +0100

    Wordbreak: Fix wordbraekdata helper script.
    Didn't work with newer versions of python.

--- src/sort_numeric_hex.py
+++ src/sort_numeric_hex.py
@@ -3,4 +3,4 @@ import sys
 
 lines = open(sys.argv[1]).readlines()
 lines_out = sorted(lines, key=lambda line: int(line.split("..")[0], 16))
-map(sys.stdout.write, lines_out)
+sys.stdout.writelines(lines_out)
