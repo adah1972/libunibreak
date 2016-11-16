f6591297eb59c80163e2a0abd6784831f66047ba
Tom Hacohen <tom@stosb.com>
2016-11-16 09:15:32 +0000

    Fix marking of mid-char (utf8/16) with regional indicators.
    There was a bug causing those not to be marked correctly when using
    regional indicators. An example broken text would be: "═🇫".
    
    This was due to a forgotten call to the function in charge of marking
    the breaks, causing it to default to 0 (break) instead of having 2
    (inside char) set.
    
    Fixes #15.

--- src/wordbreak.c
+++ src/wordbreak.c
@@ -391,6 +391,11 @@ static void set_wordbreaks(
                 set_brks_to(s, brks, posLast, posCur, len,
                             WORDBREAK_NOBREAK, get_next_char);
             }
+            else
+            {
+                set_brks_to(s, brks, posLast, posCur, len,
+                            WORDBREAK_BREAK, get_next_char);
+            }
             wbcSeqStart = wbcCur;
             posLast = posCur;
             break;
