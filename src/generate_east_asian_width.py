c94cd54d8a3598110545a85cc1a18e0b143562ff
Wu Yongwei <wuyongwei@gmail.com>
2024-02-29 14:00:23 +0800

    Extract common Python code to unicode_data_property

--- src/generate_east_asian_width.py
+++ src/generate_east_asian_width.py
@@ -1,64 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
-
-# Leading comment of the input file
-leading_comment = ""
-# This dictionary will map each start point to its corresponding end point and
-# East Asian width property
-east_asian_width_properties = {}
+import unicode_data_property
 
 
-def parse_east_asian_width_line(line):
-    # Skip comments and empty lines
-    if line.startswith('#') or not line.strip():
-        return None
-
-    # Parse the valid line to extract the range and the EAW property
-    range_part, prop = line.split(';')
-    prop = prop.split('#')[0].strip()  # Clean up the EAW property string
-
-    # Handle single code points and ranges
-    if '..' in range_part:
-        start_str, end_str = range_part.split('..')
-        start = int(start_str, 16)
-        end = int(end_str, 16)
-    else:
-        start = end = int(range_part.strip(), 16)
-
-    return start, end, prop
-
-
-def load_east_asian_width_data(file_path):
-    global leading_comment
-    global east_asian_width_properties
-
-    with open(file_path, 'r', encoding='utf-8') as input_file:
-        leading_comment = input_file.readline()
-        leading_comment += input_file.readline()
-        last_start = -1
-        last_end = -1
-        last_prop = ''
-        for line in input_file:
-            parsed_data = parse_east_asian_width_line(line)
-            if parsed_data:
-                start, end, prop = parsed_data
-                if last_end + 1 == start and last_prop == prop:
-                    east_asian_width_properties[last_start] = (end, prop)
-                    last_end = end
-                else:
-                    east_asian_width_properties[start] = (end, prop)
-                    last_start, last_end, last_prop = start, end, prop
-
-    # Sort by the start points
-    east_asian_width_properties = {
-        k: v for k, v in
-        sorted(east_asian_width_properties.items(),
-               key=lambda item: item[0])
-    }
-
-
-def output_east_asian_width_data(skip=''):
+def output_east_asian_width_data(leading_comment,
+                                 east_asian_width_properties,
+                                 skip=''):
     print('/* The content of this file is generated from:')
     print(leading_comment, end='')
     print('*/')
@@ -76,8 +24,10 @@ def output_east_asian_width_data(skip=''):
 def main():
     input_file_path = sys.argv[1] if sys.argv[1:] else \
         'EastAsianWidth.txt'
-    load_east_asian_width_data(input_file_path)
-    output_east_asian_width_data(skip='N')
+    leading_comment, east_asian_width_properties = \
+        unicode_data_property.load_data(input_file_path)
+    output_east_asian_width_data(
+        leading_comment, east_asian_width_properties, skip='N')
 
 
 if __name__ == '__main__':
--- src/generate_grapheme_break.py
+++ src/generate_grapheme_break.py
@@ -1,61 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-
-# Leading comment of the input file
-leading_comment = ""
+import unicode_data_property
 
 
-def parse_data_line(line):
-    # Skip comments and empty lines
-    if line.startswith('#') or not line.strip():
-        return None
-
-    # Parse the valid line to extract the range and the property
-    range_part, prop = line.split(';')
-    prop = prop.split('#')[0].strip()  # Clean up the property string
-
-    # Handle single code points and ranges
-    if '..' in range_part:
-        start_str, end_str = range_part.split('..')
-        start = int(start_str, 16)
-        end = int(end_str, 16)
-    else:
-        start = end = int(range_part.strip(), 16)
-
-    return start, end, prop
-
-
-def load_data(file_path):
-    global leading_comment
-    result = {}
-
-    with open(file_path, 'r', encoding='utf-8') as input_file:
-        leading_comment = input_file.readline()
-        leading_comment += input_file.readline()
-        last_start = -1
-        last_end = -1
-        last_prop = ''
-        for line in input_file:
-            parsed_data = parse_data_line(line)
-            if parsed_data:
-                start, end, prop = parsed_data
-                if last_end + 1 == start and last_prop == prop:
-                    result[last_start] = (end, prop)
-                    last_end = end
-                else:
-                    result[start] = (end, prop)
-                    last_start, last_end, last_prop = start, end, prop
-
-    # Sort by the start points
-    result = {
-        k: v for k, v in
-        sorted(result.items(), key=lambda item: item[0])
-    }
-    return result
-
-
-def output_grapheme_break_data(grapheme_break_properties):
+def output_grapheme_break_data(leading_comment, grapheme_break_properties):
     print('/* The content of this file is generated from:')
     print(leading_comment, end='')
     print('*/')
@@ -71,8 +20,9 @@ def output_grapheme_break_data(grapheme_break_properties):
 def main():
     input_file_path = sys.argv[1] if sys.argv[1:] else \
         'GraphemeBreakProperty.txt'
-    grapheme_break_properties = load_data(input_file_path)
-    output_grapheme_break_data(grapheme_break_properties)
+    leading_comment, grapheme_break_properties = \
+        unicode_data_property.load_data(input_file_path)
+    output_grapheme_break_data(leading_comment, grapheme_break_properties)
 
 
 if __name__ == '__main__':
